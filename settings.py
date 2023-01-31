89f17b4ae24e46d217ae9cb507812262f62da1db
Nhat <nate.m.luong@gmail.com>
2023-01-31 20:04:02 +0100

    add rooms and participant labels

--- settings.py
+++ settings.py
@@ -17,6 +17,81 @@ SESSION_CONFIGS = [
     },
 ]
 
+ROOMS = [
+    dict(
+        name='pilot',
+        display_name='PUEB Econ Waiting Room 0',
+        participant_label_file='_rooms/pilot.txt',
+        use_secure_urls=False
+    ),
+    dict(
+        name='session1',
+        display_name='PUEB Econ Waiting Room 1',
+        participant_label_file='_rooms/session1.txt',
+        use_secure_urls=False
+    ),
+    dict(
+        name='session2',
+        display_name='PUEB Econ Waiting Room 2',
+        participant_label_file='_rooms/session2.txt',
+        use_secure_urls=False
+    ),
+    dict(
+        name='session3',
+        display_name='PUEB Econ Waiting Room 3',
+        participant_label_file='_rooms/session3.txt',
+        use_secure_urls=False
+    ),
+    dict(
+        name='session4',
+        display_name='PUEB Econ Waiting Room 4',
+        participant_label_file='_rooms/session4.txt',
+        use_secure_urls=False
+    ),
+    dict(
+        name='session5',
+        display_name='PUEB Econ Waiting Room 5',
+        participant_label_file='_rooms/session5.txt',
+        use_secure_urls=False
+    ),
+    dict(
+        name='session6',
+        display_name='PUEB Econ Waiting Room 6',
+        participant_label_file='_rooms/session6.txt',
+        use_secure_urls=False
+    ),
+    dict(
+        name='session7',
+        display_name='PUEB Econ Waiting Room 7',
+        participant_label_file='_rooms/session7.txt',
+        use_secure_urls=False
+    ),
+    dict(
+        name='session8',
+        display_name='PUEB Econ Waiting Room 8',
+        participant_label_file='_rooms/session8.txt',
+        use_secure_urls=False
+    ),
+    dict(
+        name='session9',
+        display_name='PUEB Econ Waiting Room 9',
+        participant_label_file='_rooms/session9.txt',
+        use_secure_urls=False
+    ),
+    dict(
+        name='session10',
+        display_name='PUEB Econ Waiting Room 10',
+        participant_label_file='_rooms/session10.txt',
+        use_secure_urls=False
+    ),
+    dict(
+        name='session11',
+        display_name='PUEB Econ Waiting Room 11',
+        participant_label_file='_rooms/session11.txt',
+        use_secure_urls=False
+    )
+]
+
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
 # the session config can be accessed from methods in your apps as self.session.config,
